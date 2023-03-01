c8fa7d640de14f6eec20902bd1bf8a8f259cbec5
Ran Pol <rnd809@gmail.com>
2023-02-28 22:35:23 -0500

    Create the foundation for the slides.

--- src/components/SlidesShow.js
+++ src/components/SlidesShow.js
@@ -1,22 +1,54 @@
 import React from 'react'
 import { slides } from '../constants'
 
-const Slidesshow = () => {
-  const [currentSlideIndex, setCurrentSlideIndex] = React.useState(0)
+const SlidesShow = () => {
+  // state for the currently selected slide
+  const [currentSlide, setCurrentSlide] = React.useState(0)
+  const [expanded, setExpanded] = React.useState(false)
 
-  const handleSwipeLeft = () => {
-    setCurrentSlideIndex(currentSlideIndex - 1)
+  const slide = slides[currentSlide]
+
+  function handleSlideClick(slideIndex) {
+    setCurrentSlide(slideIndex)
+    setExpanded(true)
   }
 
-  const handleSwipeRight = () => {
-    setCurrentSlideIndex(currentSlideIndex + 1)
+  function handleCloseClick() {
+    setExpanded(false)
   }
 
   return (
-    <div>
-      <h1>Slideshow Component</h1>
+    <div className="slideshow">
+      {expanded ? (
+        <div className="slideshow__expanded">
+          <div className="slideshow__image-container">
+            <img src={slide.imageUrl} alt={slide.title} />
+          </div>
+          <div className="slideshow__content">
+            <h2>{slide.title}</h2>
+            <p>{slide.description}</p>
+            <button onClick={handleCloseClick}>Close</button>
+          </div>
+        </div>
+      ) : (
+        <>
+          <h2>Slideshow</h2>
+          <div className="slideshow__slides">
+            {slides.map((slide, index) => (
+              <div
+                className="slideshow__slide"
+                key={slide.id}
+                onClick={() => handleSlideClick(index)}
+              >
+                <img src={slide.imageUrl} alt={slide.title} />
+                <h3>{slide.title}</h3>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   )
 }
 
-export default Slidesshow
+export default SlidesShow
