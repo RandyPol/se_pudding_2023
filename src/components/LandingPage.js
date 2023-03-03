e8b307c23cdcc3e8ad4b9408b6a05e2f26db1740
Ran Pol <rnd809@gmail.com>
2023-03-03 00:42:49 -0500

    Update the title.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import TinderLogo from '../images/tinder-logo.svg'
-
+import { Link } from 'react-router-dom'
 function LandingPage() {
   return (
     <div className="landing">
       <div className="landing__containter">
         <img src={TinderLogo} alt="Tinder Logo" className="landing__logo" />
-        <h1 className="landing__heading">
-          What Millennial Users Should Expect When They Swipe?
-        </h1>
-        <button className="landing__button-enter">Find Out</button>
+        <h1 className="landing__heading">Will You Find Love Swiping Right?</h1>
+        <Link to="/dataSlide">
+          <button className="landing__button-enter">Find Out</button>
+        </Link>
       </div>
     </div>
   )
