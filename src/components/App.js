f64bf39d591719cb2caa5897c4e2eda8104c460b
Ran Pol <rnd809@gmail.com>
2023-03-03 04:42:29 -0500

    Import the React router package to use routing and switch.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React from 'react'
+// Import the routing components from react-router-dom
+import { Route, Switch, useLocation } from 'react-router-dom'
+// Import the components
 import LandingPage from './LandingPage'
+import Header from './Header'
+import Team from './Team'
+import SlidesShow from './SlidesShow'
 
 function App() {
+  const location = useLocation()
   return (
-    <div>
-      <LandingPage />
+    <div className={'page'}>
+      {location.pathname !== '/' && <Header />}
+      <Switch>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <main className="page__container">
+          <Route path="/dataSlide">
+            <SlidesShow />
+          </Route>
+          <Route path="/team">
+            <Team />
+          </Route>
+        </main>
+      </Switch>
     </div>
   )
 }
