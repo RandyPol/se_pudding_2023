b82966c629f41477029f789ebaabc9dc826e928d
Ran Pol <rnd809@gmail.com>
2023-03-03 04:34:59 -0500

    Update the MainPaper component abd add the styles.

--- src/components/MainPaper.js
+++ src/components/MainPaper.js
@@ -55,15 +55,35 @@ const MainPaper = ({ slides, handleSlideClick }) => {
       </div>
       <h2 className="introduction__headings">CONCLUSION</h2>
       <p className="introduction__paragraphs">
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using 'Content here, content here', making it
-        look like readable English. Many desktop publishing packages and web
-        page editors now use Lorem Ipsum as their default model text, and a
-        search for 'lorem ipsum' will uncover many web sites still in their
-        infancy. Various versions have evolved over the years, sometimes by
-        accident, sometimes on purpose (injected humour and the like).
+        We conducted EDA on the tinder dataset and highlighted some trends. We
+        found more relationships with those that used Tinder, compared to those
+        that did not use Tinder. This occurrence was confirmed with hypothesis
+        testing, which suggested the relationship occurrences were different
+        among Tinder users and non users.
+      </p>
+      <p className="introduction__paragraphs">
+        We then created a machine learning model to predict the relationship
+        status of an individual, based on a few self reported features.
+        Initially, the dataset was split into three groups: the full dataset,
+        tinder users, and non users. The full dataset model had the best F1
+        score, while the non users model came a close second. A good F1 score
+        suggests the model has good precision and recall, in other words, good
+        at predicting both positive and negative classes. The tinder users model
+        had an average F1 score, so the model does not accurately predict both
+        the positive and negative classes. This was due to the small nature of
+        the Tinder users dataset, which also left very little data to test the
+        model. Overall, the best model to use for predictions is the model based
+        on the full dataset.{' '}
+      </p>
+      <p className="introduction__paragraphs">
+        We tested our model with a dataset we created. The results of the model
+        predict a relationship, with a 93.99% probability.
+      </p>
+      <p className="introduction__paragraphs">
+        Contrary to popular belief that Tinder is merely a hookup app, with
+        relationships found far and few in between, our dataset suggests the
+        opposite. Among these universities, individuals tend to have better luck
+        finding a relationship using tinder.
       </p>
     </div>
   )
